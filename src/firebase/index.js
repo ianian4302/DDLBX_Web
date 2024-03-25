a68cfd042718d4cfefeb35c1388a0ed0d81a3fad
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-03-25 23:23:16 +0800

    feat: connect with firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3yrpGDzI2CdJE6Jb0xx5SgTNGHzEeek8",
+  authDomain: "ddlbx-web.firebaseapp.com",
+  projectId: "ddlbx-web",
+  storageBucket: "ddlbx-web.appspot.com",
+  messagingSenderId: "239903105303",
+  appId: "1:239903105303:web:a5271c49e15dbcc0e71752"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
