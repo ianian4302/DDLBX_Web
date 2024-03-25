4263efb36abb4a37fb32e718b84eaec808b4d15a
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-03-25 23:30:03 +0800

    fix: The background is not displayed correctly

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,46 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import Sidebar from "./components/SideBar.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <main>
+        <Sidebar />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+        <header>
+            <RouterLink class="custom-link" to="/">Home</RouterLink>
+            <RouterLink class="custom-link" to="/about">About</RouterLink>
+        </header>
+        <body>
+            <RouterView />
+        </body>
+    </main>
 </template>
 
 <style scoped>
+* {
+    background: transparent;
+}
+body {
+    margin-left: 100px;
+}
+main {
+    min-height: 100vh;
+    background: url("./assets/background_2.png") no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: multiply;
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+    justify-content: space-around;
+    padding: 1%;
+}
+.custom-link {
+    text-decoration: none;
+    color: #333;
+    font-size: 1.5rem;
 }
 </style>
