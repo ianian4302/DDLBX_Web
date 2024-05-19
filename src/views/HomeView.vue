0fe628e21fd65d7798da4ff58eade83d870d18d0
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-05-19 17:44:13 +0800

    chore: Refactor HomeView.vue to use Markdown editor and fetch content from mainpagedoc.md file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,48 @@
 <script setup>
-import Sidebar from "../components/SideBar.vue";
+import { ref } from "vue";
+import { MdPreview, MdCatalog } from "md-editor-v3";
+import Markdown from "vue3-markdown-it";
+
+const id = "preview-only";
+const text = ref("# Hello Editor");
+const scrollElement = document.documentElement;
+</script>
+<template>
+    <div class="prview">
+        <MdPreview :editorId="id" :modelValue="source" />
+        <MdCatalog :editorId="id" :scrollElement="scrollElement" />
+    </div>
+</template>
+
+
+<script>
+export default {
+    components: {
+        Markdown,
+    },
+    data() {
+        return {
+            source: " ",
+        };
+    },
+    mounted() {
+        this.fetchMarkdown(); // 在組件掛載後執行 fetchMarkdown 方法
+    },
+    methods: {
+        fetchMarkdown() {
+            fetch("./mainpagedoc.md") // 發送請求
+                .then((res) => res.text()) // 將回應轉為文本格式
+                .then((res) => {
+                    this.source = res; // 將回應設置到 data 中的 source 屬性中，Vue 會自動更新到畫面上
+                })
+                .catch((error) => {
+                    console.error("Fetch error:", error);
+                });
+        },
+    },
+};
 </script>
 
-<template>
-    <div class="home"></div>
-</template>
-<style></style>
+<style>
+
+</style>
