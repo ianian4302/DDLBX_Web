77fc9bf94b41c176bb96503cc57348c4a6b6e60f
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-05-19 17:44:04 +0800

    chore: Refactor Test.vue component to use Markdown editor and fetch content from test.md file

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,116 +1,47 @@
-<script>
-import { createApp, ref } from "vue";
-import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-} from "@google/generative-ai";
+<script setup>
+import { ref } from "vue";
+import { MdPreview, MdCatalog } from "md-editor-v3";
+import Markdown from "vue3-markdown-it";
 
-const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = "AIzaSyAnpXYIZ4EqRlogVuxIa62_rlGQZPrpT8g";
-const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: MODEL_NAME });
-
-const generationConfig = {
-    temperature: 0.9,
-    topK: 1,
-    topP: 1,
-    maxOutputTokens: 2048,
-};
-
-const safetySettings = [
-    {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-];
-
-const generatedText = ref("");
-
-async function generateText() {
-    const parts = [{ text: "說你好" }]; // 可以根據需要更改文字部分
-    const result = await model.generateContent({
-        contents: [{ role: "user", parts }],
-        generationConfig,
-        safetySettings,
-    });
-    generatedText.value = result.response.text();
-}
-
-const input_message = ref("");
-const compileMarkdown = ref("");
-function say(message) {
-    try {
-        compileMarkdown = marked(input_message);
-        alert(message);
-    } catch (e) {
-        console.log(e);
-    }
-}
-//test code
-const inputText = ref("");
-const resultText = ref("");
-function handleKeyDown(event) {
-    if (event.key === "Enter") {
-        addCharacter();
-    }
-}
-function addCharacter() {
-    resultText.value += inputText.value;
-    inputText.value = ""; // 清空輸入框
-}
+const id = "preview-only";
+const text = ref("# Hello Editor");
+const scrollElement = document.documentElement;
 </script>
-
 <template>
-    <span>Multiline message is:</span>
-    <p></p>
-    <div class="input">
-        <div>1231232123ultiline messag</div>
-        <textarea
-            v-model="input_message"
-            placeholder="code input here"
-        ></textarea>
-        <textarea
-            v-model="compileMarkdown"
-            placeholder="code output here"
-        ></textarea>
-    </div>
-
-    <p></p>
-    <div>
-        <button @click="generateText">生成文字</button>
-        <div v-if="generatedText">{{ generatedText }}</div>
+    <div class="prview">
+        <MdPreview :editorId="id" :modelValue="source" />
+        <MdCatalog :editorId="id" :scrollElement="scrollElement" />
     </div>
 </template>
 
-<style>
-textarea {
-    border: none;
-    border-right: 1px solid #ccc;
-    resize: none;
-    outline: none;
-    background-color: #f6f6f6;
-    font-size: 14px;
-    font-family: Verdana, Arial, Helvetica, sans-serif;
-    padding: 20px;
-}
-input {
-    font-family: "Lucida Console", Courier, monospace;
-}
+<script>
+export default {
+    components: {
+        Markdown,
+    },
+    data() {
+        return {
+            source: " ",
+        };
+    },
+    mounted() {
+        this.fetchMarkdown(); // 在組件掛載後執行 fetchMarkdown 方法
+    },
+    methods: {
+        fetchMarkdown() {
+            fetch("./test.md") // 發送請求
+                .then((res) => res.text()) // 將回應轉為文本格式
+                .then((res) => {
+                    this.source = res; // 將回應設置到 data 中的 source 屬性中，Vue 會自動更新到畫面上
+                })
+                .catch((error) => {
+                    console.error("Fetch error:", error);
+                });
+        },
+    },
+};
+</script>
 
-code {
-    color: #f66;
-}
+<style>
+
 </style>
