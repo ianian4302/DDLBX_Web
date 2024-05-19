a890e74f24ad0b919551387a8de8eb7d90683732
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-05-19 17:44:20 +0800

    chore: Add Contribute.vue component with Markdown editor and fetch content from contribute.md file

---
+++ src/views/Contribute.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="prview">
+        <MdPreview :editorId="id" :modelValue="source" />
+        <MdCatalog :editorId="id" :scrollElement="scrollElement" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { MdPreview, MdCatalog } from "md-editor-v3";
+import Markdown from "vue3-markdown-it";
+
+const id = "preview-only";
+const text = ref("# Hello Editor");
+const scrollElement = document.documentElement;
+</script>
+
+<script>
+export default {
+    components: {
+        Markdown,
+    },
+    data() {
+        return {
+            source: " ",
+        };
+    },
+    mounted() {
+        this.fetchMarkdown(); // 在組件掛載後執行 fetchMarkdown 方法
+    },
+    methods: {
+        fetchMarkdown() {
+            fetch("./contribute.md") // 發送請求
+                .then((res) => res.text()) // 將回應轉為文本格式
+                .then((res) => {
+                    this.source = res; // 將回應設置到 data 中的 source 屬性中，Vue 會自動更新到畫面上
+                })
+                .catch((error) => {
+                    console.error("Fetch error:", error);
+                });
+        },
+    },
+};
+</script>
+
+<style>
+</style>
